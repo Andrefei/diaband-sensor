c73509ac659aca165c0fe33eda4520a9c7002a3b
andrefei <andreas.eikeland@hotmail.com>
2018-12-01 16:39:15 -0800

    Add support for socket.io communication

--- index.js
+++ index.js
@@ -1,23 +1,42 @@
-'use strict';
+//Send sensor data to React server
+//by using the socket.io engine
 
-//
-//	Load our C++ app as a module
-//
+'use strict';
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+const port = process.env.PORT || 4001;
+const index = require("./routes/index");
 let addon = require('./build/Release/integer');
 
-//
-//	Create two variables as buffers
-//
+//Create Socket.io server
+const app = express();
+app.use(index);
+
+const server = http.createServer(app);
+const io = socketIo(server);
+
+//Buffer variables
 let nrOne = new Buffer("1");
 let nrTwo = new Buffer("40000");
 
-//
 //	Call the C++ function with our numbers, and store the result in a new
 //	variable
-//
 let sum = addon.sum(nrOne, nrTwo);
 
-//
-//	Display the result of the C++ calculation
-//
-console.log("The sum of %s and %s is: %s", nrOne, nrTwo, sum);
+//Sends data to client on connection
+io.on("connection", socket => {
+  console.log("New client connected"), setInterval(
+    () => emitData(socket),
+    10000
+  );
+  socket.on("disconnect", () => console.log("Client disconnected"));
+});
+const emitData = async socket => {
+  try {
+    socket.emit("FromAPI", sum);
+  } catch (error) {
+    console.error(`Error: ${error.code}`);
+  }
+};
+server.listen(port, () => console.log(`Listening on port ${port}`));
