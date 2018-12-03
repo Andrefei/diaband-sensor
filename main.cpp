21d69970a1b032e5e31d8852dfa88e86dd951f92
andrefei <andreas.eikeland@hotmail.com>
2018-12-03 10:19:24 -0800

    Remove some functionality from main.cpp to make it run

--- main.cpp
+++ main.cpp
@@ -31,100 +31,7 @@ void onTagDeparture(void){
 }
 
 int main(int argc, char ** argv) {
-    int res = 0x00;
-    int i;
 
-    g_TagCB.onTagArrival = onTagArrival;
-    g_TagCB.onTagDeparture = onTagDeparture;
-    nfcManager_doInitialize();
-    nfcManager_registerTagCallback(&g_TagCB);
-    nfcManager_enableDiscovery(DEFAULT_NFA_TECH_MASK, 0x01, 0, 0);
-
-    printf("\n-------------\nWaiting for tag...\n");
-    do{
-        pthread_cond_wait(&condition, &mutex);
-
-        /* Raw access to tag */
-        switch (g_tagInfos.technology)
-        {
-            case TARGET_TYPE_MIFARE_UL:
-            {
-                unsigned char ReadCmd[] = {0x30U, /*block*/ 0x00};
-                unsigned char Resp[16];
-                printf("MifareUL Read command: ");
-                for(i = 0x00; i < (unsigned int) sizeof(ReadCmd) ; i++) printf("%02X ", ReadCmd[i]); printf("\n");
-                res = nfcTag_transceive(g_tagInfos.handle, ReadCmd, sizeof(ReadCmd), Resp, sizeof(Resp), 500);
-                if(0x00 == res) {
-                    printf("RAW Tag transceive failed\n");
-                }
-                else {
-                    printf("MifareUL Read command sent - Response: ");
-                    for(i = 0x00; i < (unsigned int)res; i++) printf("%02X ", Resp[i]); printf("\n\n");
-                }
-            } break;
-
-            case TARGET_TYPE_MIFARE_CLASSIC:
-            {
-                unsigned char AuthCmd[] = {0x60U, /*block*/ 0x01, 0x00, 0x00, 0x00, 0x00, /*key*/ 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
-                unsigned char ReadCmd[] = {0x30U, /*block*/ 0x01};
-                unsigned char Resp[20];
-                printf("MifareClassic Authenticate command: ");
-                for(i = 0x00; i < (unsigned int) sizeof(AuthCmd) ; i++) printf("%02X ", AuthCmd[i]); printf("\n");
-                res = nfcTag_transceive(g_tagInfos.handle, AuthCmd, 12, Resp, sizeof(Resp), 500);
-                if(0x00 == res)
-                {
-                    printf("\n\t\tRAW Tag transceive failed\n");
-                }
-                else {
-                    printf("MifareClassic Authenticate command sent - Response: ");
-                    for(i = 0x00; i < (unsigned int)res; i++) printf("%02X ", Resp[i]); printf("\n\n");
-                }
-                printf("MifareUL Read command: ");
-                for(i = 0x00; i < (unsigned int) sizeof(ReadCmd) ; i++) printf("%02X ", ReadCmd[i]); printf("\n");
-                res = nfcTag_transceive(g_tagInfos.handle, ReadCmd, sizeof(ReadCmd), Resp, sizeof(Resp), 500);
-                if(0x00 == res) {
-                    printf("RAW Tag transceive failed\n");
-                }
-                else {
-                    printf("MifareUL Read command sent - Response: ");
-                    for(i = 0x00; i < (unsigned int)res; i++) printf("%02X ", Resp[i]); printf("\n\n");
-                }
-            } break;
-
-            case TARGET_TYPE_ISO15693:
-            {
-		/*
-		res = nfcTag_isFormatable(g_tagInfos.handle);
-		if (res == 1) { printf("Formatable\n"); }
-		else { printf("Not Formatable\n"); }
-		printf("res Return: %i\n", res);
-		res = nfcTag_formatTag(g_tagInfos.handle);
-		printf("res formatTag: %i\n", res);
-		*/
-		//unsigned char ReadCmd[] = {};
-                unsigned char ReadCmd[] = {0x02U, 0x20, /*block*/ 0x04};
-                //unsigned char WriteCmd[] = {0x02U, 0x21, /*block*/ 0x04, 0x11, 0x22, 0x33, 0x44};
-                unsigned char Resp[255];
-                printf("ISO15693 Read command: ");
-                for(i = 0x00; i < (unsigned int) sizeof(ReadCmd) ; i++) printf("%02X ", ReadCmd[i]); printf("\n");
-                res = nfcTag_transceive(g_tagInfos.handle, ReadCmd, sizeof(ReadCmd), Resp, 32, 3000);
-                if(0x00 == res) {
-                    printf("RAW Tag transceive failed\n");
-                }
-                else {
-                    printf("ISO15693 Read command sent - Response: ");
-                    for(i = 0x00; i < (unsigned int)res; i++) printf("%02X ", Resp[i]); printf("\n\n");
-                }
-
-            } break;
-
-            default:
-            {
-                printf("Unsupported tag type\n");
-            } break;
-        }
-    }while(1);
-    nfcManager_doDeinitialize();
 }
 
 //
@@ -132,9 +39,96 @@ int main(int argc, char ** argv) {
 //
 NAN_METHOD(Sum)
 {
-	main(0, NULL);
+	int res = 0x00;
+	int i;
+
+	g_TagCB.onTagArrival = onTagArrival;
+	g_TagCB.onTagDeparture = onTagDeparture;
+	nfcManager_doInitialize();
+	nfcManager_registerTagCallback(&g_TagCB);
+	nfcManager_enableDiscovery(DEFAULT_NFA_TECH_MASK, 0x01, 0, 0);
+
+	printf("\n-------------\nWaiting for tag...\n");
+	do{
+			pthread_cond_wait(&condition, &mutex);
+
+			/* Raw access to tag */
+			printf("Tag read and the tech is: ");
+			printf(" %s\n", g_tagInfos.technology);
+			/*
+			switch (g_tagInfos.technology)
+			{
+					case TARGET_TYPE_MIFARE_UL:
+					{
+							unsigned char ReadCmd[] = {0x30U, 0x00};
+							unsigned char Resp[16];
+							printf("MifareUL Read command: ");
+							for(i = 0x00; i < (unsigned int) sizeof(ReadCmd) ; i++) printf("%02X ", ReadCmd[i]); printf("\n");
+							res = nfcTag_transceive(g_tagInfos.handle, ReadCmd, sizeof(ReadCmd), Resp, sizeof(Resp), 500);
+							if(0x00 == res) {
+									printf("RAW Tag transceive failed\n");
+							}
+							else {
+									printf("MifareUL Read command sent - Response: ");
+									for(i = 0x00; i < (unsigned int)res; i++) printf("%02X ", Resp[i]); printf("\n\n");
+							}
+					} break;
+
+					case TARGET_TYPE_MIFARE_CLASSIC:
+					{
+							unsigned char AuthCmd[] = {0x60U, 0x01, 0x00, 0x00, 0x00, 0x00, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+							unsigned char ReadCmd[] = {0x30U, 0x01};
+							unsigned char Resp[20];
+							printf("MifareClassic Authenticate command: ");
+							for(i = 0x00; i < (unsigned int) sizeof(AuthCmd) ; i++) printf("%02X ", AuthCmd[i]); printf("\n");
+							res = nfcTag_transceive(g_tagInfos.handle, AuthCmd, 12, Resp, sizeof(Resp), 500);
+							if(0x00 == res)
+							{
+									printf("\n\t\tRAW Tag transceive failed\n");
+							}
+							else {
+									printf("MifareClassic Authenticate command sent - Response: ");
+									for(i = 0x00; i < (unsigned int)res; i++) printf("%02X ", Resp[i]); printf("\n\n");
+							}
+							printf("MifareUL Read command: ");
+							for(i = 0x00; i < (unsigned int) sizeof(ReadCmd) ; i++) printf("%02X ", ReadCmd[i]); printf("\n");
+							res = nfcTag_transceive(g_tagInfos.handle, ReadCmd, sizeof(ReadCmd), Resp, sizeof(Resp), 500);
+							if(0x00 == res) {
+									printf("RAW Tag transceive failed\n");
+							}
+							else {
+									printf("MifareUL Read command sent - Response: ");
+									for(i = 0x00; i < (unsigned int)res; i++) printf("%02X ", Resp[i]); printf("\n\n");
+							}
+					} break;
+
+					case TARGET_TYPE_ISO15693:
+					{
+							unsigned char ReadCmd[] = {0x02U, 0x20, 0x04};
+							unsigned char Resp[255];
+							printf("ISO15693 Read command: ");
+							for(i = 0x00; i < (unsigned int) sizeof(ReadCmd) ; i++) printf("%02X ", ReadCmd[i]); printf("\n");
+							res = nfcTag_transceive(g_tagInfos.handle, ReadCmd, sizeof(ReadCmd), Resp, 32, 3000);
+							if(0x00 == res) {
+									printf("RAW Tag transceive failed\n");
+							}
+							else {
+									printf("ISO15693 Read command sent - Response: ");
+									for(i = 0x00; i < (unsigned int)res; i++) printf("%02X ", Resp[i]); printf("\n\n");
+							}
+
+					} break;
+
+					default:
+					{
+							printf("Unsupported tag type\n");
+					} break;
+			}
+			*/
+	}while(1);
+	nfcManager_doDeinitialize();
 	//
-	//	1.	Save the buffers that I passed from NodeJS in to local variables
+	//	1.	Save the buffers that passed from NodeJS in to local variables
 	//
 	unsigned int nrOne = info[0]->Uint32Value();
 	unsigned int nrDwo = info[1]->Uint32Value();
