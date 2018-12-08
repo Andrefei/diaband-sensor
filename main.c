85512f1e2f04fcdbefcd03db2dfc57609373ca53
andrefei <andreas.eikeland@hotmail.com>
2018-12-08 14:06:18 -0800

    First test with timed wait

--- main.c
+++ main.c
@@ -4,6 +4,9 @@
 #include <ctype.h>
 #include <string.h>
 #include <pthread.h>
+#define _OPEN_THREADS
+#include <time.h>
+#include <errno.h>
 #include "linux_nfc_api.h"
 
 pthread_cond_t condition = PTHREAD_COND_INITIALIZER;
@@ -22,31 +25,65 @@ void onTagDeparture(void){
 }
 
 int main(int argc, char ** argv) {
-    int res = 0x00;
-    int i;
+  int res = 0x00;
+  int i;
 
-    g_TagCB.onTagArrival = onTagArrival;
-    g_TagCB.onTagDeparture = onTagDeparture;
+  g_TagCB.onTagArrival = onTagArrival;
+  g_TagCB.onTagDeparture = onTagDeparture;
 
-    nfcManager_doInitialize();
-    nfcManager_registerTagCallback(&g_TagCB);
-    nfcManager_enableDiscovery(DEFAULT_NFA_TECH_MASK, 0x01, 0, 0);
+  nfcManager_doInitialize();
+  nfcManager_registerTagCallback(&g_TagCB);
+  nfcManager_enableDiscovery(DEFAULT_NFA_TECH_MASK, 0x01, 0, 0);
+  //pthread_cond_t cond;
+  //pthread_mutex_t mutex;
+  time_t T;
+  struct timespec t;
 
-    pthread_cond_wait(&condition, &mutex);
-    // The only case for our project is ISO15693
-    if (g_tagInfos.technology == TARGET_TYPE_ISO15693) {
-      unsigned char ReadCmd[] = {0x02U, 0x20, /*block*/ 0x04};
-      unsigned char Resp[255];
-      res = nfcTag_transceive(g_tagInfos.handle, ReadCmd, sizeof(ReadCmd), Resp, 32, 3000);
-      if(0x00 == res) {
-          printf("RAW Tag transceive failed\n");
-      }
-      else {
-          // Raw access to tag succeeded
-          for(i = 0x00; i < (unsigned int)res; i++) printf("%02X ", Resp[i]); printf("\n\n");
-      }
+  if (pthread_mutex_init(&mutex, NULL) != 0) {
+    perror("pthread_mutex_init() error");
+    exit(1);
+  }
+
+  if (pthread_cond_init(&condition, NULL) != 0) {
+    perror("pthread_cond_init() error");
+    exit(2);
+  }
+
+  if (pthread_mutex_lock(&mutex) != 0) {
+    perror("pthread_mutex_lock() error");
+    exit(3);
+  }
+
+  time(&T);
+  t.tv_sec = T + 2;
+  printf("starting timedwait at %s", ctime(&T));
+  if (pthread_cond_timedwait(&condition, &mutex, &t) != 0)
+    if (errno == EAGAIN)
+      puts("wait timed out");
+    else {
+      perror("pthread_cond_timedwait() error");
+      exit(4);
     }
 
-    nfcManager_doDeinitialize();
-    return 0;
+  time(&T);
+  printf("timedwait over at %s", ctime(&T));
+
+
+  //pthread_cond_wait(&condition, &mutex);
+  // The only case for our project is ISO15693
+  if (g_tagInfos.technology == TARGET_TYPE_ISO15693) {
+    unsigned char ReadCmd[] = {0x02U, 0x20, /*block*/ 0x04};
+    unsigned char Resp[255];
+    res = nfcTag_transceive(g_tagInfos.handle, ReadCmd, sizeof(ReadCmd), Resp, 32, 3000);
+    if(0x00 == res) {
+        printf("RAW Tag transceive failed\n");
+    }
+    else {
+        // Raw access to tag succeeded
+        for(i = 0x00; i < (unsigned int)res; i++) printf("%02X ", Resp[i]); printf("\n\n");
+    }
+  }
+
+  nfcManager_doDeinitialize();
+  return 0;
 }
