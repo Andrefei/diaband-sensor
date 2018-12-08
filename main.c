93eec6ebb2eca8a26e2ff20713cbd272e7839ebd
andrefei <andreas.eikeland@hotmail.com>
2018-12-08 14:23:57 -0800

    Fix comments

--- main.c
+++ main.c
@@ -16,6 +16,7 @@ nfcTagCallback_t g_TagCB;
 nfc_tag_info_t g_tagInfos;
 
 void onTagArrival(nfc_tag_info_t *pTagInfo){
+    // Stop waiting when tag is detected
     g_tagInfos = *pTagInfo;
     pthread_cond_signal(&condition);
 }
@@ -27,6 +28,8 @@ void onTagDeparture(void){
 int main(int argc, char ** argv) {
   int res = 0x00;
   int i;
+  time_t T;
+  struct timespec t;
 
   g_TagCB.onTagArrival = onTagArrival;
   g_TagCB.onTagDeparture = onTagDeparture;
@@ -34,45 +37,13 @@ int main(int argc, char ** argv) {
   nfcManager_doInitialize();
   nfcManager_registerTagCallback(&g_TagCB);
   nfcManager_enableDiscovery(DEFAULT_NFA_TECH_MASK, 0x01, 0, 0);
-  //pthread_cond_t cond;
-  //pthread_mutex_t mutex;
-  time_t T;
-  struct timespec t;
-  /*
-  if (pthread_mutex_init(&mutex, NULL) != 0) {
-    perror("pthread_mutex_init() error");
-    exit(1);
-  }
 
-  if (pthread_cond_init(&condition, NULL) != 0) {
-    perror("pthread_cond_init() error");
-    exit(2);
-  }
-
-  if (pthread_mutex_lock(&mutex) != 0) {
-    perror("pthread_mutex_lock() error");
-    exit(3);
-  }
- */
+  // Wait for tag for two ms
   time(&T);
   t.tv_sec = T + 2;
-  printf("starting timedwait at %s", ctime(&T));
   pthread_cond_timedwait(&condition, &mutex, &t);
-  /*
-  if (pthread_cond_timedwait(&condition, &mutex, &t) != 0)
-    if (errno == EAGAIN)
-      puts("wait timed out");
-    else {
-      perror("pthread_cond_timedwait() error");
-      exit(4);
-    }
-  */
-  time(&T);
-  printf("timedwait over at %s", ctime(&T));
 
-
-  //pthread_cond_wait(&condition, &mutex);
-  // The only case for our project is ISO15693
+  // The only tag our project supports is ISO15693
   if (g_tagInfos.technology == TARGET_TYPE_ISO15693) {
     unsigned char ReadCmd[] = {0x02U, 0x20, /*block*/ 0x04};
     unsigned char Resp[255];
